2fdf95c60755b4dc38166d8026f82c8039e6b06b
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 15:39:20 +0100

    feat(leave): add DTOs for leave types and balances

---
+++ LOMS_Leave/LOMS_Leave_Shared/LeaveBalanceDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Leave_Shared
+{
+    public class LeaveBalanceDTO
+    {
+        public int LeaveBalanceID { get; set; }
+        public int EmployeeID { get; set; }
+        public int LeaveTypeID { get; set; }
+        public int Year { get; set; }
+        public decimal EntitledDays { get; set; }
+        public decimal UsedDays { get; set; }
+        public decimal RemainingDays { get; set; }
+    }
+}
---
+++ LOMS_Leave/LOMS_Leave_Shared/LeaveTypeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Leave_Shared
+{
+    public class LeaveTypeDTO
+    {
+        public int LeaveTypeID { get; set; }
+        public string LeaveName { get; set; }
+        public bool IsPaid { get; set; }
+        public short MaxDaysPerYear { get; set; }
+        public bool RequiresDocument {  get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
