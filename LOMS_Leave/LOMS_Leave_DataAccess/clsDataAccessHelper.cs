c2492dc5e141d9156fe3e9eda1bd38c2cabcbbfb
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 15:38:15 +0100

    feat(leave): setup data access foundation and helpers

---
+++ LOMS_Leave/LOMS_Leave_DataAccess/clsDataAccessHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Leave_DataAccess
+{
+    public  class clsDataAccessHelper
+    {
+        public static object EnsureValue(object value)
+        {
+            return value ?? DBNull.Value;
+        }
+    }
+}
