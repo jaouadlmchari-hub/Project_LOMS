dd25484d052cb7f8261cc60c37569dffeed43a31
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 16:17:09 +0100

    fix(auth): improve employee service client communication

--- LOMS_Auth/LOMS_Auth_DataAccess/clsEmployeeServiceClient.cs
+++ LOMS_Auth/LOMS_Auth_DataAccess/clsEmployeeServiceClient.cs
@@ -16,20 +16,39 @@ namespace LOMS_Auth_DataAccess
             private static readonly HttpClient _httpClient = new HttpClient();
 
             // Appelle l'API du microservice Employee
-            public static async Task<dynamic> GetEmployeeBasicInfoAsync(int EmployeeID)
+
+            public static string BaseUrl { get; set; } = "http://localhost:7175";
+            public static async Task<EmployeeBasicInfoDTO?> GetEmployeeBasicInfoAsync(int EmployeeID)
             {
+                // On garde cette ligne pour Docker si  n'as pas de certificat valide
+
                 System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
-                //string url = $"https://localhost:7175/api/employees/{EmployeeID}";
-                //string url = $"https://host.docker.internal:7175/api/employees/{EmployeeID}";
-                string url = $"http://employee-container:8080/api/employees/{EmployeeID}";
-
-                var response = await _httpClient.GetAsync(url);
-                if (response.IsSuccessStatusCode)
+                string url = $"{BaseUrl.TrimEnd('/')}/api/employees/{EmployeeID}";
+                try
                 {
-                    var json = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<EmployeeBasicInfoDTO>(json);
+                    var response = await _httpClient.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+
+                        // On force Newtonsoft à être tolérant sur la casse (Majuscules/Minuscules)
+                        var settings = new JsonSerializerSettings
+                        {
+                            NullValueHandling = NullValueHandling.Ignore,
+                            MissingMemberHandling = MissingMemberHandling.Ignore
+                        };
+
+                        return JsonConvert.DeserializeObject<EmployeeBasicInfoDTO>(json, settings);
+                    }
                 }
+                catch (Exception ex)
+                {
+                    // Debug : voir l'erreur exacte dans les logs Docker
+                    Console.WriteLine($"[API Error]: {ex.Message}");
+                }
+
                 return null;
             }
         }
