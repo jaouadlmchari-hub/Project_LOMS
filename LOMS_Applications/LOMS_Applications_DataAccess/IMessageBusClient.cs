3c8aadf6fa1946dcc17c573f503da40bfa1ab0e1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 02:23:13 +0100

    feat(apps): add interface for message bus client abstraction

---
+++ LOMS_Applications/LOMS_Applications_DataAccess/IMessageBusClient.cs
@@ -0,0 +1,14 @@
+﻿using LOMS_Applications_Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Applications_DataAccess
+{
+    public interface IMessageBusClient
+    {
+        void PublishApplicationCreated(ApplicationCreatedEvent eventModel);
+    }
+}
