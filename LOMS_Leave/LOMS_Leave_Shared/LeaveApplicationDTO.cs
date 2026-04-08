57d7bc62d7503c931bc3be5b6916a6a22011a75d
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 15:38:49 +0100

    feat(leave): define core DTOs for leave applications

---
+++ LOMS_Leave/LOMS_Leave_Shared/LeaveApplicationDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Leave_Shared
+{
+    public class LeaveApplicationDTO
+    {
+        public int LeaveApplicationID { get; set; }
+        public int ApplicationID { get; set; }
+        public int LeaveTypeID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Decimal NumberOfDays { get; set; }
+        public string Reason { get; set; }
+
+    }
+}
