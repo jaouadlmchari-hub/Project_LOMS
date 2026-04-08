ca86cc0d989d615f4f034a775e576bdb22348805
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 02:24:32 +0100

    refactor(apps): update data access helper for improved stability

--- LOMS_Applications/LOMS_Applications_DataAccess/clsDataAccessHelper.cs
+++ LOMS_Applications/LOMS_Applications_DataAccess/clsDataAccessHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LOMS_Employee_DataAccess
+namespace LOMS_Applications_DataAccess
 {
     public  class clsDataAccessHelper
     {
