c6aecd41ff885b314f8cdf7f131f4d5da17c84e2
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 02:22:47 +0100

    feat(apps): define ApplicationCreatedEvent DTO for kafka messaging

---
+++ LOMS_Applications/LOMS_Applications_Shared/ApplicationCreatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Applications_Shared
+{
+    public class ApplicationCreatedEvent
+    {
+        public int ApplicationID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public string Status { get; set; }
+        public DateTime LastStatusDate { get; set; }
+        public int CreatedByUserID { get; set; }
+        public int ApplicationTypeID { get; set; }
+        public string Notes { get; set; }
+        public int ActionByUserID { get; set; } = -1;
+    }
+}
