8209b035211ca2c5116cc42f3a20dc8a378d9741
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-08 02:23:39 +0100

    feat(apps): implement kafka producer using Confluent.Kafka

---
+++ LOMS_Applications/LOMS_Applications_DataAccess/clsKafkaProducer.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using LOMS_Applications_Shared;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LOMS_Applications_DataAccess
+{
+    public static class clsKafkaProducer
+    {
+        private static readonly ProducerConfig _config = new ProducerConfig
+        {
+            BootstrapServers = "kafka-container:9092",
+            Acks = Acks.All 
+        };
+
+        public static async Task<bool> PublishApplicationCreatedAsync(ApplicationCreatedEvent eventModel)
+        {
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                try
+                {
+                    string messageValue = JsonSerializer.Serialize(eventModel);
+
+                    var result = await producer.ProduceAsync("applications-topic",
+                        new Message<Null, string> { Value = messageValue });
+
+                    return result.Status == PersistenceStatus.Persisted;
+                }
+                catch (System.Exception)
+                {
+                    return false; // L'envoi a échoué
+                }
+            }
+        }
+    }
+}
