8be8734c935e733d61ffd9044dfb6aa41022c1c4
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-18 21:10:34 +0100

    feat(shared): add SalaryResponseDTO for inter-service communication

---
+++ LOMS_Employee/LOMS_Employee_Shared/SalaryResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOMS_Employee_Shared
+{
+    public class SalaryResponseDTO
+    {
+        public decimal Salary { get; set; }
+
+        public int EmployeeID { get; set; }
+    }
+}
