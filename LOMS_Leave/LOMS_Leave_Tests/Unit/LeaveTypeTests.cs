fdd4a67c699b14ff8efe1ede4d0d2bf88700b0b1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 00:31:31 +0100

    test(leave): add xUnit project with Unit and Integration folders

---
+++ LOMS_Leave/LOMS_Leave_Tests/Unit/LeaveTypeTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using FluentAssertions;
+using LOMS_Leave_Buisness;
+using LOMS_Leave_Shared;
+
+namespace LOMS_Leave_Tests
+{
+    public class LeaveTypeTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeWithDefaultValues()
+        {
+            // Act
+            var leaveType = new clsLeaveType();
+
+            // Assert (Vérifie que l'initialisation AddNew est correcte)
+            leaveType.LeaveTypeDTO.Should().NotBeNull();
+            leaveType.LeaveTypeDTO.LeaveTypeID.Should().Be(-1);
+            leaveType.LeaveTypeDTO.IsActive.Should().BeTrue();
+        }
+
+        [Fact]
+        public void LeaveType_ShouldHoldDataCorrectly()
+        {
+            // Arrange
+            var leaveType = new clsLeaveType();
+
+            // Act
+            leaveType.LeaveTypeDTO.LeaveName = "Annual Leave";
+            leaveType.LeaveTypeDTO.MaxDaysPerYear = 25;
+
+            // Assert
+            leaveType.LeaveTypeDTO.LeaveName.Should().Be("Annual Leave");
+            leaveType.LeaveTypeDTO.MaxDaysPerYear.Should().Be(25);
+        }
+    }
+}
