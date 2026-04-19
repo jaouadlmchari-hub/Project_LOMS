a479f8420cab1c226e11baf06d7f01df1964a0de
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:01:43 +0100

    test(employee): add integration tests for department lifecycle

---
+++ LOMS_Employee/LOMS_Employee_Tests/Integration/clsDepartment_IntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using LOMS_Employee_Business;
+using System.Data;
+
+namespace LOMS_Employee_Tests.Integration
+{
+    public class clsDepartment_IntegrationTests
+    {
+        [Fact]
+        public void Save_ShouldAddNewDepartment_And_Find_ShouldReturnIt()
+        {
+            // --- ARRANGE ---
+            var dept = new clsDepartment();
+            dept.DTO.DepartmentName = "IT Department Test";
+            dept.DTO.IsActive = true;
+
+            // --- ACT ---
+            bool saveResult = dept.Save();
+            int id = dept.DTO.DepartmentID;
+
+            // --- ASSERT ---
+            Assert.True(saveResult);
+            Assert.True(id > 0);
+
+            var foundDept = clsDepartment.Find(id);
+            Assert.NotNull(foundDept);
+            Assert.Equal("IT Department Test", foundDept.DTO.DepartmentName);
+        }
+
+        [Fact]
+        public void GetDepartmentSummary_ShouldReturnData()
+        {
+            // Act
+            DataTable dt = clsDepartment.GetDepartmentSummary();
+
+            // Assert
+            Assert.NotNull(dt);
+        }
+    }
+}
