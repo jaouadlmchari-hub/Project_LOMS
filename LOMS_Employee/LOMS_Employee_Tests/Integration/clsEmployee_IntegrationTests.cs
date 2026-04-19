98e5fdb1f25992b0d8a087f9b1ed16ac1c22c34a
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:01:45 +0100

    test(employee): add integration tests for employee creation and orchestration

---
+++ LOMS_Employee/LOMS_Employee_Tests/Integration/clsEmployee_IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using LOMS_Employee_Business;
+using System.Threading.Tasks;
+
+namespace LOMS_Employee_Tests.Integration
+{
+    public class clsEmployee_IntegrationTests
+    {
+        [Fact]
+        public async Task SaveAsync_ShouldCreateEmployeeAndReturnTrue()
+        {
+            // Arrange
+            var emp = new clsEmployee();
+            emp.DTO.FirstName = "Test";
+            emp.DTO.LastName = "User";
+            emp.DTO.NationalNo = "T12345";
+            emp.DTO.Email = "test@domain.com";
+            emp.DTO.DepartmentID = 1;
+            emp.DTO.JobTitleID = 1;
+
+            // Act
+            bool result = await emp.SaveAsync();
+
+            // Assert
+            Assert.True(result);
+            Assert.NotEqual(-1, emp.DTO.EmployeeID);
+
+            // Cleanup
+            if (result) await clsEmployee.DeleteEmployeeAsync(emp.DTO.EmployeeID);
+        }
+    }
+}
