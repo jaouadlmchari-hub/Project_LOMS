a54e3c7302e3a02aea024a4c8294b54da97d8f34
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:01:19 +0100

    test(auth): add integration tests for user persistence and role assignment

---
+++ LOMS_Auth/LOMS_Auth_Tests/Integration/clsUser_IntegrationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOMS_Auth_Tests.Integration
+{
+    internal class clsUser_IntegrationTests
+    {
+    }
+}
