b8cee7f88c9e0ad797147bd53ea4b616442076ed
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:00:42 +0100

    test(app): add unit tests for application defaults and business logic

---
+++ LOMS_Applications/LOMS_Applications_Tests/Unit/clsApplication_UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using LOMS_Applications_Buisness;
+using System;
+
+namespace LOMS_Applications_Tests.Unit
+{
+    public class clsApplication_UnitTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetDefaultValues()
+        {
+            // Act
+            var app = new clsApplication();
+
+            // Assert
+            Assert.Equal("Pending", app.DTO.Status);
+            Assert.Equal(clsApplication.enMode.AddNew, app.Mode);
+            Assert.Equal(DateTime.Now.Date, app.DTO.ApplicationDate.Date);
+        }
+    }
+}
