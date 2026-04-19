c0e5b3d28d155c13d078912ed62598555d2709a1
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:01:15 +0100

    test(auth): add unit tests for user object initialization

---
+++ LOMS_Auth/LOMS_Auth_Tests/Unit/clsUser_UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using LOMS_Auth_Business;
+
+namespace LOMS_Auth_Tests.Unit
+{
+    public class clsUser_UnitTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeWithDefaultValues()
+        {
+            // Act
+            var user = new clsUser();
+
+            // Assert
+            Assert.Equal(-1, user.DTO.UserID);
+            Assert.Equal(clsUser.enMode.AddNew, user.Mode);
+            Assert.Null(user.EmployeeInfo);
+        }
+    }
+}
