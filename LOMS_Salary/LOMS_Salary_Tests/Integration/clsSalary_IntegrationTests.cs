290f7eb6b5f4a8229259e1299e28a195b66041ea
jaouad El m'chari <jawadlmchari@gmail.com>
2026-04-19 03:01:57 +0100

    test(salary): add integration tests for salary data retrieval

---
+++ LOMS_Salary/LOMS_Salary_Tests/Integration/clsSalary_IntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using LOMS_Salary_Buisness;
+
+namespace LOMS_Salary_Tests.Integration
+{
+    public class clsSalary_IntegrationTests
+    {
+        [Fact]
+        public void FindByEmployeeID_ShouldReturnSalary_IfEmployeeExists()
+        {
+            // Arrange
+            int existingEmployeeID = 1;
+
+            // Act
+            var salary = clsSalary.FindByEmployeeID(existingEmployeeID);
+
+            // Assert
+            if (salary != null)
+            {
+                Assert.NotNull(salary.DTO);
+                Assert.True(salary.DTO.Salary >= 0);
+            }
+        }
+    }
+}
