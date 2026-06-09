1bb3486f79b6fda9fbb7ec80264a5bde4a203676
jaouad El m'chari <jawadlmchari@gmail.com>
2026-06-09 15:27:19 +0100

    fix: resolve git merge conflicts in vite.config.js

--- LOMS_Web/vite.config.js
+++ LOMS_Web/vite.config.js
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// Si tailwindcss est importé via un plugin Vite, assurez-vous qu'il est installé,
-// sinon vous pouvez enlever "tailwindcss()" de la liste des plugins.
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    allowedHosts: true,
-  },
-});
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // <--- C'est CRUCIAL pour que Docker puisse accéder à React
-    port: 5174  // <--- Assure-toi que le port correspond à celui dans nginx.conf
-  }
-})
->>>>>>> feat/employee-ui-infra-update
+    host: true, // Crucial pour l'accès Docker
+    port: 5174, // Doit correspondre à la configuration de ta passerelle Nginx
+  },
+});
